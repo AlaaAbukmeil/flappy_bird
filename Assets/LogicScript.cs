4263d991d0df9e48516bd71a209b2ddfd93ccac3
Alaa Abukmeil <abukmeilalaa@gmail.com>
2025-12-28 21:57:39 +0800

    new design

---
+++ Assets/LogicScript.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LogicScript : MonoBehaviour
+{
+    public int playerScore = 0;
+    public Text scoreText;
+    [ContextMenu("Increase Score")]
+    public void AddScore()
+    {
+        playerScore++;
+        scoreText.text = playerScore.ToString();
+    }
+}
